35c0b011db476409b896a25865325c0f7ca47b2c
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-02-26 21:14:32 -0500

    Add header to about page

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,5 +2,5 @@ import React from 'react';
 import {Link} from 'react-router-dom'
 
 export default (props) => {
-  return <Link to='/'><div className='header'>{props.children}</div></Link>
+  return <Link to='/'><div className='header'><span aria-label='' role='img'>{props.children}</span></div></Link>
 }
