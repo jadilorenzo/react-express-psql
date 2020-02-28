a76d6be608176ef594d9280d3546e86dd3fc2791
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-02-27 21:11:36 -0500

    Add form to new user page

--- src/components/CreateUserPage.js
+++ src/components/CreateUserPage.js
@@ -2,11 +2,11 @@ import React, {useState} from 'react';
 import {Redirect} from 'react-router-dom'
 import Api from '../Api'
 import {v4} from 'uuid'
+import Form from './Form'
 
 
 function CreateUserPage() {
   const [redirect, setRedirect] = useState(false)
-  const [passcode, setPasscode] = useState('1234')
   const [uuid, setUuid] = useState('')
   const [username, setUsername] = useState('New User Name')
   const [showWarning, setShowWarning] = useState(false)
@@ -20,39 +20,19 @@ function CreateUserPage() {
     <div className="h-screen w-screen bg-gray-100">
       <div className={`header`}>Create User Page</div>
       <div className={`body-section`}>
-        <form onSubmit={() => {
-            const uid = v4()
-            Api.post('users', {uid, name: username, passcode: Number.parseFloat(passcode)})
-            setUuid(uid)
-            setRedirect(true)
-        }}>
+        <Form onMessage={(input) => {
+          setUsername(input)
+        }} theme='none' submitName='none' type='name'>
           Username
-          <input
-            type='text'
-            value={username}
-            className={`input border focus:border-red-500`}
-            onChange={(e) => {
-              setUsername(e.target.value)
-            }}
-            onClick={() => {
-              setShowWarning(true)
-            }}/>
+        </Form>
+        <Form onSubmit={({input}) => {
+          const uid = v4()
+          Api.post('users', {uid, name: username, passcode: Number.parseFloat(input)})
+          setUuid(uid)
+          setRedirect(true)
+        }} theme='white' submitName='Create' type='password'>
           Four Digit Passcode
-          <input
-            type='password'
-            value={passcode}
-            className={`input w-32 border focus:border-red-500`}
-            onChange={(e) => {
-              setPasscode(e.target.value)
-            }}
-            onClick={() => {
-              setShowWarning(true)
-            }}
-          />
-        {showWarning ? <div className='text-red-500'>All fields are required!</div> : <div/>}
-        <hr/>
-        <button className={`button`}>Create</button>
-        </form>
+        </Form>
       </div>
     </div>
   );
