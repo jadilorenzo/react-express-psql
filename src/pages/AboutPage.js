3bde6874cf77ad902f392e3a7f2846f466fcddf2
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-02-29 20:51:13 -0500

    gitignore update

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
-import Header from '../components/Header'
-import react from '../components/icon/react.svg'
-import tailwind from '../components/icon/tailwind.svg'
-import postgres from '../components/icon/postgres.png'
+import { Link } from 'react-router-dom';
+import Header from '../components/Header';
+import react from '../components/icon/react.svg';
+import tailwind from '../components/icon/tailwind.svg';
+import postgres from '../components/icon/postgres.png';
 
 function AboutPage() {
   return (
@@ -11,32 +11,32 @@ function AboutPage() {
       <Header>
         Resources
       </Header>
-      <div className='block md:flex'>
-        <div className={`body-section md:w-1/3`}>
-          <div className=''>
-            <img alt='tailwind' className='mx-auto shadow bg-gray-300 rounded-full w-32 h-32' src={tailwind}></img>
-            <div className='mt-4 mx-auto text-center'>Made with Tailwind CSS</div>
+      <div className="block md:flex">
+        <div className="body-section md:w-1/3">
+          <div className="">
+            <img alt="tailwind" className="mx-auto shadow bg-gray-300 rounded-full w-32 h-32" src={tailwind} />
+            <div className="mt-4 mx-auto text-center">Made with Tailwind CSS</div>
           </div>
         </div>
-        <div className={`body-section md:w-1/3`}>
-          <div className=''>
-            <img alt='react' className='mx-auto shadow bg-gray-300 rounded-full w-32 h-32' src={react}></img>
-            <div className='mt-4 mx-auto text-center'>Made with React</div>
+        <div className="body-section md:w-1/3">
+          <div className="">
+            <img alt="react" className="mx-auto shadow bg-gray-300 rounded-full w-32 h-32" src={react} />
+            <div className="mt-4 mx-auto text-center">Made with React</div>
           </div>
         </div>
-        <div className={`body-section md:w-1/3`}>
-          <div className=''>
-            <div className='mx-auto shadow bg-gray-300 rounded-full w-32 h-32'><img alt='postgres' className='w-16 m-auto pt-8' src={postgres}></img></div>
-            <div className='mt-4 mx-auto text-center'>Made with Postgres</div>
+        <div className="body-section md:w-1/3">
+          <div className="">
+            <div className="mx-auto shadow bg-gray-300 rounded-full w-32 h-32"><img alt="postgres" className="w-16 m-auto pt-8" src={postgres} /></div>
+            <div className="mt-4 mx-auto text-center">Made with Postgres</div>
           </div>
         </div>
       </div>
-      <Link to='/'>
-        <div className={`body-section text-center transition duration-500 ease-in-out hover:shadow-xl`}>
+      <Link to="/">
+        <div className="body-section text-center transition duration-500 ease-in-out hover:shadow-xl">
           Home
         </div>
       </Link>
-      <br/>
+      <br />
     </div>
   );
 }
--- src/pages/CreateRoomPage.js
+++ src/pages/CreateRoomPage.js
@@ -1,26 +1,27 @@
-import React, {useEffect, useState} from 'react';
-import Api from '../Api'
-import {Redirect, useParams} from 'react-router-dom'
-import {v4} from 'uuid'
-import Form from '../components/Form'
-import Header from '../components/Header'
+import React, { useEffect, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
+import { v4 } from 'uuid';
+import Api from '../Api';
+import Form from '../components/Form';
+import Header from '../components/Header';
 
 function CreateRoomPage() {
-  const [redirect, setRedirect] = useState(false)
-  const [unaccessibleRooms, setUnaccessibleRooms] = useState([])
+  const [redirect, setRedirect] = useState(false);
+  const [unaccessibleRooms, setUnaccessibleRooms] = useState([]);
 
-  const params = useParams()
+  const params = useParams();
 
   useEffect(() => {
-    Api.get('rooms').then(({rooms}) => {
-      const filteredRooms = rooms.filter(room => !(JSON.parse(room.users.replace('{', '[').replace('}', ']')).includes(params.userId)))
-      setUnaccessibleRooms(filteredRooms)
-    })
-  }, [params.userId])
+    Api.get('rooms').then(({ rooms }) => {
+      const filteredRooms = rooms.filter((room) => !(JSON.parse(room.users.replace('{', '[').replace('}', ']')).includes(params.userId)));
+      setUnaccessibleRooms(filteredRooms);
+    });
+  }, [params.userId]);
 
   if (redirect) {
-    return <Redirect to={`/messages/${params.userId}`}/>
+    return <Redirect to={`/messages/${params.userId}`} />;
   }
+
   console.log(unaccessibleRooms);
 
   return (
@@ -28,20 +29,31 @@ function CreateRoomPage() {
       <Header>
         Create Room +
       </Header>
-      <div className={`body-section`}>
-        <Form button='white' submitName='Create' onSubmit={({input}) => {
-          setRedirect(true)
-          Api.post('rooms', {rid: v4(), name: input, users: [params.userId]})
-        }}>
+      <div className="body-section">
+        <Form
+          button="white"
+          submitName="Create"
+          onSubmit={({ input }) => {
+            setRedirect(true);
+            Api.post('rooms', { rid: v4(), name: input, users: [params.userId] });
+          }}
+        >
           Room Name
         </Form>
       </div>
-      <div className={`body-section`}>
+      <div className="body-section">
         Join Existing Room...
-        {unaccessibleRooms.map(x => <div onClick={() => {
-          Api.post('roomAddPerson', {users: [...JSON.parse(x.users.replace('{', '[').replace('}', ']')), params.userId], rid: x.rid})
-          setRedirect(true)
-        }} className={`bubble transition duration-500 ease-in-out hover:shadow-md`}>{x.name}</div>)}
+        {unaccessibleRooms.map((x) => (
+          <div
+            onClick={() => {
+              Api.post('roomAddPerson', { users: [...JSON.parse(x.users.replace('{', '[').replace('}', ']')), params.userId], rid: x.rid });
+              setRedirect(true);
+            }}
+            className="bubble transition duration-500 ease-in-out hover:shadow-md"
+          >
+            {x.name}
+          </div>
+        ))}
       </div>
     </div>
   );
