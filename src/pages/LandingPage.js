79652c4558a3c4f71e4aa2da39c6644ed89821ea
Jacob Di Lorenzo <46056370+jadilorenzo@users.noreply.github.com>
2020-03-22 09:09:18 -0400

    Instructs user how to use the site

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -4,13 +4,47 @@ import Header from '../components/Header'
 import login from '../components/icon/login.svg'
 import menu from '../components/icon/menu.svg'
 import userAdd from '../components/icon/user-add.svg'
+import community from '../components/icon/communication.png'
+import createUser from '../components/icon/create-user.png'
+import createRoom from '../components/icon/create-room.png'
+import sendMessages from '../components/icon/send-messages.png'
+
 
 function LandingPage() {
   return (
-    <div className="h-screen w-screen">
+    <div className="h-screen w-screen landing-page overflow-scroll">
       <Header>
-        Messages
+        Online Chat
       </Header>
+      <div className='body-section'>
+        <img className='mx-auto mt-4' alt='' src={community}/>
+        <div className='text-blue-700 text-center text-2xl'>Create your community with Online Chat!</div>
+
+        <div className='m-4 flex shadow w-2/3 mx-auto rounded-md'>
+          <img className='inline-block bg-gray-200 w-1/2 h-full rounded-tl-md rounded-bl-md' alt='' src={createUser}/>
+          <div className='p-4 bg-gray-100 rounded-tr-md rounded-br-md'>
+            <div className='step'> Step 1:</div>
+            <div className='step'> Create a user with your username and four digit passcode.</div>
+          </div>
+        </div>
+
+        <div className='m-4 flex shadow w-2/3 mx-auto rounded-md'>
+          <img className='inline-block bg-gray-200 w-1/2 h-full rounded-tl-md rounded-bl-md' alt='' src={createRoom}/>
+          <div className='p-4 bg-gray-100 rounded-tr-md rounded-br-md'>
+            <div className='step'> Step 2:</div>
+            <div className='step'> Create a room with the name of your choice.</div>
+          </div>
+        </div>
+
+        <div className='m-4 flex shadow w-2/3 mx-auto rounded-md'>
+          <img className='inline-block bg-gray-200 w-1/2 h-full rounded-tl-md rounded-bl-md' alt='' src={sendMessages}/>
+          <div className='p-4 bg-gray-100 rounded-tr-md rounded-br-md'>
+            <div className='step'> Step 3:</div>
+            <div className='step'> Start sending messages. Tell people about your life, your adventures and your passions!</div>
+          </div>
+        </div>
+
+      </div>
       <div className='block'>
         <div className=''>
           <Link to='/login'>
